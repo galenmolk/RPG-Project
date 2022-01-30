1a981d43510d7b327fe1e2ba24382d06a88a7f81
Galen Molk <galenmolk@gmail.com>
2022-01-30 10:41:37 -0500

    Initial commit

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Camera mainCamera;
+    private Animator animator;
+
+    private const string FORWARD_SPEED_PARAM = "forwardSpeed";
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        bool hasHit = Physics.Raycast(ray, out RaycastHit hit);
+        if (hasHit)
+        {
+            agent.destination = hit.point;
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = agent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        animator.SetFloat(FORWARD_SPEED_PARAM, speed);
+    }
+}
